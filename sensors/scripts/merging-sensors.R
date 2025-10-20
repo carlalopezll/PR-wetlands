9568613256233e01dfadfe69ff23fc7e53314766
Carla López Lloreda <carla.lpez09@gmail.com>
2025-10-20 16:43:39 -0400

    only merging winter 25 and summer 24 data

--- sensors/scripts/merging-sensors.R
+++ sensors/scripts/merging-sensors.R
@@ -18,6 +18,13 @@ cond_to_winter$site <- "TO"
 cond_pa_summer$site <- "PA"
 cond_pa_winter$site <- "PA"
 
+# add season
+cond_to_summer$season <- "summer"
+cond_to_winter$season <- "winter"
+
+cond_pa_summer$season <- "summer"
+cond_pa_winter$season <- "winter"
+
 # merge datasets
 
 # function that ignores the different column names
@@ -42,18 +49,16 @@ do_2021 <- read_csv("sensors/data/DO/DO_all sites_Summer2021.csv")
 # read in DO data
 
 # merging summer 2024 and winter 2025 data
-to_winter24 <- read_csv("sensors/data/DO/raw data/Winter 2024/Tortuguero_DO_Winter2024.csv")
-pa_winter24 <- read_csv("sensors/data/DO/raw data/Winter 2024/Palmas_DO_Winter2024.csv")
+# to_winter24 <- read_csv("sensors/data/DO/DO_Tortuguero_Winter2024.csv")
+# pa_winter24 <- read_csv("sensors/data/DO/DO_Palmas_Winter2024.csv")
 
-to_summer24 <- read_csv("sensors/data/DO/raw data/Summer 2024/Tortuguero_DO_Summer2024.csv")
-pa_summer24 <- read_csv("sensors/data/DO/raw data/Summer 2024/Palmas_DO_Summer2024.csv")
+to_summer24 <- read_csv("sensors/data/DO/DO_Tortuguero_Summer2024.csv")
+pa_summer24 <- read_csv("sensors/data/DO/DO_Palmas_Summer2024.csv")
 
-to_winter25 <- read_csv("sensors/data/DO/raw data/Winter 2025/Tortuguero_DO_Winter2025.csv")
-pa_winter25 <- read_csv("sensors/data/DO/raw data/Winter 2025/Palmas_DO_Winter2025.csv")
+to_winter25 <- read_csv("sensors/data/DO/DO_Tortuguero_Winter2025.csv")
+pa_winter25 <- read_csv("sensors/data/DO/DO_Palmas_Winter2025.csv")
 
 # add site names
-to_winter24$site <- "TO"
-pa_winter24$site <- "PA"
 
 to_winter25$site <- "TO"
 pa_winter25$site <- "PA"
@@ -61,13 +66,20 @@ pa_winter25$site <- "PA"
 to_summer24$site <- "TO"
 pa_summer24$site <- "PA"
 
+# add seasons
+to_winter25$season <- "winter"
+pa_winter25$season <- "winter"
+
+to_summer24$season <- "summer"
+pa_summer24$season <- "summer"
+
 # function that ignores the different column names
-force_bind = function(df1, df2, df3, df4, df5, df6) {
-  colnames(df6) = colnames(df5) = colnames(df4) = colnames(df3) = colnames(df2) = colnames(df1)
-  bind_rows(df1, df2, df3, df4, df5, df6)
+force_bind = function(df1, df2, df3, df4) {
+  colnames(df4) = colnames(df3) = colnames(df2) = colnames(df1)
+  bind_rows(df1, df2, df3, df4)
 }
 
-do <- force_bind(to_winter24, pa_winter24, to_winter25, pa_winter25, to_summer24, pa_summer24)
+do <- force_bind(to_winter25, pa_winter25, to_summer24, pa_summer24)
 
 # rename columns
 do <- do %>%
@@ -77,6 +89,3 @@ do <- do %>%
          temp_C = Temperature)
 
 write.csv(do, "sensors/data/DO/merged_do.csv", row.names = FALSE)
-
-# merge cond and do data
-
